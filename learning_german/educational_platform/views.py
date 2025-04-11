758f6df23d064e48e2412c0c23f07b6e25aa90fc
BitmanBT <gainullin.ii@phystech.edu>
2025-04-11 23:57:10 +0300

    Added exercise functionality

--- learning_german/educational_platform/views.py
+++ learning_german/educational_platform/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from random import sample, shuffle
 from .models import Word
 from .forms import AddWordForm
 
@@ -22,7 +23,63 @@ def vocabulary(request):
     return render(request, 'educational_platform/vocabulary.html', context)
 
 def exercises(request):
-    return render(request, 'educational_platform/exercises.html')
+    all_words = list(Word.objects.all())
+    
+    if not all_words:
+        return render(request, 'educational_platform/exercises.html', {'no_words': True})
+    
+    context = {}
+    
+    if request.method == 'POST' and 'selected_option_id' in request.POST:
+        selected_id = int(request.POST['selected_option_id'])
+        correct_id = int(request.POST['correct_word_id'])
+        correct_word = Word.objects.get(id=correct_id)
+        
+        context.update({
+            'direction': request.POST['direction'],
+            'word_to_translate': request.POST['word_to_translate'],
+            'options': [
+                {
+                    'id': int(id),
+                    'text': text,
+                    'is_correct': int(id) == correct_id
+                }
+                for id, text in zip(
+                    request.POST.getlist('option_ids'),
+                    request.POST.getlist('option_texts')
+                )
+            ],
+            'feedback': {
+                'is_correct': selected_id == correct_id,
+                'correct_answer': correct_word.russian_translation if request.POST['direction'] == 'de-ru' else correct_word.german_word,
+                'selected_id': selected_id
+            },
+            'show_result': True
+        })
+    else:
+        correct_word = sample(all_words, 1)[0]
+        other_words = [w for w in all_words if w.id != correct_word.id]
+        wrong_options = sample(other_words, min(3, len(other_words)))
+        options = [correct_word] + wrong_options
+        shuffle(options)
+        
+        direction = 'de-ru' if sample([True, False], 1)[0] else 'ru-de'
+        
+        context.update({
+            'direction': direction,
+            'word_to_translate': correct_word.german_word if direction == 'de-ru' else correct_word.russian_translation,
+            'options': [
+                {
+                    'id': word.id,
+                    'text': word.russian_translation if direction == 'de-ru' else word.german_word,
+                    'is_correct': word.id == correct_word.id
+                }
+                for word in options
+            ],
+            'correct_word_id': correct_word.id,
+            'show_result': False
+        })
+    return render(request, 'educational_platform/exercises.html', context)
 
 def add_words(request):
     if request.method == 'POST':
